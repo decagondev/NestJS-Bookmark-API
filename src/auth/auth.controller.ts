9c7279ecd92e82988d1ffbfdddab1ee58e62f293
Tom Tarpey <tomtarpeydev@gmail.com>
2024-01-03 02:41:03 +0000

    added controller skel

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Post } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 
-@Controller()
+@Controller("auth")
 export class AuthController {
     constructor(private authService : AuthService) {}
+
+    @Post("sugnup")
+    signup() {}
+
+    @Post("signin")
+    signin() {}
 }
