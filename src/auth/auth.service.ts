0c438deb1b236ebb816cc11e2e8a8aef84ba1267
Tom Tarpey <tomtarpeydev@gmail.com>
2024-01-04 19:42:34 +0000

    added some exception handling for invalid credentials and duplicate user

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,29 +1,45 @@
-import { Injectable } from "@nestjs/common";
+import { ForbiddenException, Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
 import { AuthDto } from "./dto";
 import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 
 @Injectable({})
 export class AuthService {
-    constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService) {}
 
-    async signup(dto: AuthDto) {
-        const hash = await argon.hash(dto.password);
+  async signup(dto: AuthDto) {
+    try {
+      const hash = await argon.hash(dto.password);
 
-        const user = await this.prisma.user.create({
-            data: {
-                email: dto.email,
-                hash
-            }
-        });
+      const user = await this.prisma.user.create({
+          data: {
+              email: dto.email,
+              hash
+          }
+      });
 
-        // TODO: add more elegant solution (Transforms) later
-        delete user.hash;
+      // TODO: add more elegant solution (Transforms) later
+      delete user.hash;
 
-        return user;
+      return user;
+    } catch (error) {
+      if (
+        error instanceof
+        PrismaClientKnownRequestError
+      ) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException(
+            'Credentials taken',
+          );
+        }
+      }
+      throw error;
     }
+  }
+    
 
-    signin(dto: AuthDto) {
-        return { message: "signin from the service" };
-    }
+  signin(dto: AuthDto) {
+    return { message: "signin from the service" };
+  }
 }
