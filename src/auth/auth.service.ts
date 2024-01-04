c6c10981760d2acc274519d1b5425bf53bd876ac
Tom Tarpey <tomtarpeydev@gmail.com>
2024-01-04 19:15:06 +0000

    added AuthDto to AuthService methods

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,15 @@
 import { Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
+import { AuthDto } from "./dto";
 
 @Injectable({})
 export class AuthService {
     constructor(private prisma: PrismaService) {}
-    
-    signin() {
+
+    signin(dto: AuthDto) {
         return { message: "signin from the service" };
     }
-    signup() {
+    signup(dto: AuthDto) {
         return { message: "signup from the service"};
     }
 }
