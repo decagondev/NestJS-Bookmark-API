945b96bbae11565279ac431cbf7b4316dc647c95
Tom Tarpey <tomtarpeydev@gmail.com>
2024-01-04 19:23:58 +0000

    add logic to signup a new user adding user to db

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -8,11 +8,11 @@ export class AuthController {
 
     @Post("signup")
     signup(@Body() dto: AuthDto) {
-        return this.authService.signup();
+        return this.authService.signup(dto);
     }
 
     @Post("signin")
-    signin() {
-        return this.authService.signin();
+    signin(@Body() dto: AuthDto) {
+        return this.authService.signin(dto);
     }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,26 @@
 import { Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
 import { AuthDto } from "./dto";
+import * as argon from 'argon2';
 
 @Injectable({})
 export class AuthService {
     constructor(private prisma: PrismaService) {}
 
+    async signup(dto: AuthDto) {
+        const hash = await argon.hash(dto.password);
+
+        const user = await this.prisma.user.create({
+            data: {
+                email: dto.email,
+                hash
+            }
+        });
+
+        return user;
+    }
+    
     signin(dto: AuthDto) {
         return { message: "signin from the service" };
-    }
-    signup(dto: AuthDto) {
-        return { message: "signup from the service"};
     }
 }
