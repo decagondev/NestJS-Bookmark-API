2d082eccdfdc686b3d113613264ee9fd19f5cab8
Tom Tarpey <tomtarpeydev@gmail.com>
2024-01-04 15:43:58 +0000

    added prisma service to auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
 
 @Injectable({})
 export class AuthService {
+    constructor(private prisma: PrismaService) {}
+    
     signin() {
         return { message: "signin from the service" };
     }
