1d3ad4e01a85c57d2d71e90c27c8f8010629d433
Tom Tarpey <tomtarpeydev@gmail.com>
2024-01-04 00:05:39 +0000

    update signup test in jest

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -15,10 +15,10 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  it('/ (POST)', () => {
     return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+      .get('/auth/signup')
+      .expect(201)
+      .expect({ message: "signup from the service" });
   });
 });
