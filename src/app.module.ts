0f097aca27625c457c5a1f45f6ed0a4aa3c166f1
Tom Tarpey <tomtarpeydev@gmail.com>
2024-01-04 22:09:20 +0000

    using config module to seperate env vars

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,9 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, UserModule, BookmarkModule, PrismaModule],
+  imports: [AuthModule, UserModule, BookmarkModule, PrismaModule, ConfigModule.forRoot({ isGlobal: true })],
 })
 export class AppModule {}
