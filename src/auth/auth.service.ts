7a6b3de441540a7727161c273ee25e6ff2be6363
Tom Tarpey <tomtarpeydev@gmail.com>
2024-01-04 21:34:18 +0000

    created initial signin logic

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -39,7 +39,21 @@ export class AuthService {
   }
     
 
-  signin(dto: AuthDto) {
-    return { message: "signin from the service" };
+  async signin(dto: AuthDto) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email
+      }
+    });
+    
+    if(!user) throw new ForbiddenException("Credentials incorrect");
+
+    const pwMatches = await argon.verify(user.hash, dto.password);
+    
+    if(!pwMatches) throw new ForbiddenException("Credentials incorrect");
+
+    delete user.hash;
+
+    return user;
   }
 }
