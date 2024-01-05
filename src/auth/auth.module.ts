8a7686402d1c33e29f1ceb5a71e1818dc7e9e8ab
Tom Tarpey <tomtarpeydev@gmail.com>
2024-01-05 15:51:12 +0000

    Configured JwtStrategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,11 @@ import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
 import { PrismaService } from "src/prisma/prisma.service";
 import { JwtModule } from "@nestjs/jwt";
+import { JwtStrategy } from "./strategy";
 
 @Module({
     imports: [JwtModule.register({})],
     controllers: [AuthController],
-    providers: [AuthService, PrismaService]
+    providers: [AuthService, PrismaService, JwtStrategy]
 })
 export class AuthModule {}
