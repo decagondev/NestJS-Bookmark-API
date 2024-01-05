a6bb28149c3b453de98856c9cf6efeae34ec1ddb
Tom Tarpey <tomtarpeydev@gmail.com>
2024-01-05 16:13:36 +0000

    setup initial Barer Token validation to allow a dashboard / protected routes

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 
-@Controller('user')
-export class UserController {}
+@Controller('users')
+export class UserController {
+    @UseGuards(AuthGuard('jwt'))
+    @Get('me')
+    getMe() {
+        return 'user info';
+    }
+}
