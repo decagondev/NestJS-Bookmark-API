8f42fcb1eb440459c618255dfd78454b69542bb6
Tom Tarpey <tomtarpeydev@gmail.com>
2024-01-06 05:03:16 +0000

    added custom jwd guard

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,12 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Request } from 'express';
+import { JwtGuard } from 'src/auth/guard';
 
 @Controller('users')
 export class UserController {
-    @UseGuards(AuthGuard('jwt'))
-    @Get('me')
-    getMe() {
-        return 'user info';
+    @UseGuards(JwtGuard)
+    @Get('dash')
+    getDashboard(@Req() req: Request) {
+        return req.user;
     }
 }
