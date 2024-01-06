ae17d1031e8c72b045e4eb63ec8921f709dcfc7a
Tom Tarpey <tomtarpeydev@gmail.com>
2024-01-06 05:20:36 +0000

    created a custom decorator to clean up user controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get, Req, UseGuards } from '@nestjs/common';
-import { Request } from 'express';
+import { User } from '@prisma/client';
+import { GetUser } from 'src/auth/decorator';
 import { JwtGuard } from 'src/auth/guard';
 
+@UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
-    @UseGuards(JwtGuard)
     @Get('dash')
-    getDashboard(@Req() req: Request) {
-        return req.user;
+    getDashboard(@GetUser() user: User) {
+        return user;
     }
 }
