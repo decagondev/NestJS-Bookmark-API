b3349508c0762b34eb0f401943f73d5f08fc0df9
Tom Tarpey <tomtarpeydev@gmail.com>
2024-01-07 00:43:03 +0000

    add a db clean function

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -13,4 +13,8 @@ export class PrismaService extends PrismaClient {
             }
         })
     }
+
+    cleanDb() {
+        return this.$transaction([this.bookmark.deleteMany(), this.user.deleteMany()]);
+    }
 }
