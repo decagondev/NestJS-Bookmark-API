816e746d56721b6e20af2c5c3643f4d6ddc38874
Tom Tarpey <tomtarpeydev@gmail.com>
2024-01-07 03:12:59 +0000

    added a skeleton for Crud in the bookmark controller

--- src/bookmark/bookmark.controller.ts
+++ src/bookmark/bookmark.controller.ts
@@ -1,7 +1,28 @@
-import { Controller, UseGuards } from '@nestjs/common';
+import { Controller, Delete, Get, Patch, Post, UseGuards } from '@nestjs/common';
 import { JwtGuard } from '../auth/guard';
+import { BookmarkService } from './bookmark.service';
 
 
 @UseGuards(JwtGuard)
 @Controller('bookmarks')
-export class BookmarkController {}
+export class BookmarkController {
+    
+    constructor(private bookmarkService: BookmarkService) {}
+    
+    @Post()
+    createBookmark() {}
+    
+    @Get()
+    getBookmark() {}
+
+    @Get()
+    getBookmarksById() {}
+
+    @Patch()
+    editBookmarkById() {}
+
+    @Delete()
+    deleteBookmarkById() {}
+
+    
+}
