14db4da520b4affbe7545d455cf996893fc8998b
Tom Tarpey <tomtarpeydev@gmail.com>
2024-01-07 15:00:25 +0000

    implemented createBookmark in bookmark service

--- src/bookmark/bookmark.service.ts
+++ src/bookmark/bookmark.service.ts
@@ -1,11 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
+import { CreateBookmarkDto } from './dto';
 
 @Injectable()
 export class BookmarkService {
     constructor(private prisma: PrismaService) {}
 
-    async createBookmark(userId: number) {}
+    async createBookmark(userId: number, dto: CreateBookmarkDto) {
+        const bookmark = await this.prisma.bookmark.create({ data: { userId, ...dto} });
+        return bookmark;
+    }
     
     getBookmarks(userId: number) {
         return this.prisma.bookmark.findMany({ where: { userId } });
