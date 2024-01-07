f6a47dc2b9f2b998849b84f6cec4ef7e27d8b4fe
Tom Tarpey <tomtarpeydev@gmail.com>
2024-01-07 01:28:36 +0000

    added signin e2e tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -35,26 +35,41 @@ describe('APP e2e', () => {
 
       it('Should Throw if password missing', () => {
         return pactum.spec().post('/auth/signup').withBody({ email: dto.email }).expectStatus(400);
-      })
-      
+      });
+
       it('Should Throw if no body', () => {
         return pactum.spec().post('/auth/signup').expectStatus(400);
-      })
+      });
 
       it('Should Signup', () => {
         return pactum.spec().post('/auth/signup').withBody(dto).expectStatus(201);
       });
     });
+
     describe('Signin', () => {
+      it('Should Throw if email missing', () => {
+        return pactum.spec().post('/auth/signin').withBody({ password: dto.password }).expectStatus(400);
+      });
+      
+      it('Should Throw if password missing', () => {
+        return pactum.spec().post('/auth/signin').withBody({ email: dto.email }).expectStatus(400);
+      });
+      
+      it('Should Throw if no body', () => {
+        return pactum.spec().post('/auth/signin').expectStatus(400);
+      });
+
       it('Should Signin', () => {
         return pactum.spec().post('/auth/signin').withBody(dto).expectStatus(200);
       });
     });
   });
+
   describe('User', () => {
     describe('Get user', () => {});
     describe('Edit user', () => {});
   });
+  
   describe('Bookmark', () => {
     describe('Get empty bookmarks', () => {});
     describe('Create bookmark', () => {});
