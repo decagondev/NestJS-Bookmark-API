981b306258813b135ce4f5ed698f103c87f750b0
Tom Tarpey <tomtarpeydev@gmail.com>
2024-01-07 01:10:32 +0000

    seup pactum and done logic for Should signup e2e test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,7 +1,9 @@
 import { Test } from '@nestjs/testing';
+import * as pactum from 'pactum';
 import { AppModule } from '../src/app.module';
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { PrismaService } from '../src/prisma/prisma.service';
+import { AuthDto } from 'src/auth/dto';
 describe('APP e2e', () => {
 
   let app: INestApplication;
@@ -13,17 +15,23 @@ describe('APP e2e', () => {
     app = moduleRef.createNestApplication();
     app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
     await app.init();
+    await app.listen(3333);
 
     prisma = app.get(PrismaService);
 
     await prisma.cleanDb();
+    pactum.request.setBaseUrl('http://localhost:3333');
   });
 
   afterAll(() => app.close());
 
   describe('Auth', () => {
     describe('Signup', () => {
-      it.todo("Should Signup");
+      it('Should Signup"', () => {
+        const dto : AuthDto = { email: "tom@decadev.co.uk", password: "123" };
+
+        return pactum.spec().post('/auth/signup').withBody(dto).expectStatus(201);
+      });
     });
     describe('Signin', () => {
       it.todo("Should Signin");
