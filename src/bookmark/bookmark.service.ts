077133d4e6bf9a9afe073034a2caadba301c08c8
Tom Tarpey <tomtarpeydev@gmail.com>
2024-01-07 15:06:20 +0000

    edit bookmark in bookmark service

--- src/bookmark/bookmark.service.ts
+++ src/bookmark/bookmark.service.ts
@@ -1,6 +1,6 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
-import { CreateBookmarkDto } from './dto';
+import { CreateBookmarkDto, EditBookmarkDto } from './dto';
 
 @Injectable()
 export class BookmarkService {
@@ -17,7 +17,14 @@ export class BookmarkService {
 
     getBookmarksById(userId: number, bookmarkId: number) {}
 
-    editBookmarkById(userId: number, bookmarkId: number) {}
+    async editBookmarkById(userId: number, bookmarkId: number, dto: EditBookmarkDto) {
+        const bookmark = await this.prisma.bookmark.findUnique({ where: { id: bookmarkId } });
+  
+        if (!bookmark || bookmark.userId !== userId)
+            throw new ForbiddenException('Access to resources denied');
+
+        return this.prisma.bookmark.update({ where: { id: bookmarkId }, data: { ...dto } });
+    }
 
     deleteBookmarkById(userId: number, bookmarkId: number) {}
 }
