02c12ef295b82ae7844214456001e0ce8d08ab71
Tom Tarpey <tomtarpeydev@gmail.com>
2024-01-07 00:47:09 +0000

    add the prisma db clean logic to e2e tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,15 +1,22 @@
 import { Test } from '@nestjs/testing';
 import { AppModule } from '../src/app.module';
 import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 describe('APP e2e', () => {
 
   let app: INestApplication;
+  let prisma: PrismaService;
+
   beforeAll(async () => {
     
     const moduleRef = await Test.createTestingModule({ imports: [AppModule]}).compile();
     app = moduleRef.createNestApplication();
     app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
     await app.init();
+
+    prisma = app.get(PrismaService);
+
+    await prisma.cleanDb();
   });
 
   afterAll(() => app.close());
