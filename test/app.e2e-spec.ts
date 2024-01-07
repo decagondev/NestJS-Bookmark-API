7df213ee744bf84bb4d83e5706eb3431405a2c90
Tom Tarpey <tomtarpeydev@gmail.com>
2024-01-07 00:58:33 +0000

    setup e2e test structure

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,7 +1,7 @@
 import { Test } from '@nestjs/testing';
 import { AppModule } from '../src/app.module';
 import { INestApplication, ValidationPipe } from '@nestjs/common';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaService } from '../src/prisma/prisma.service';
 describe('APP e2e', () => {
 
   let app: INestApplication;
@@ -20,5 +20,27 @@ describe('APP e2e', () => {
   });
 
   afterAll(() => app.close());
+
+  describe('Auth', () => {
+    describe('Signup', () => {
+      it.todo("Should Signup");
+    });
+    describe('Signin', () => {
+      it.todo("Should Signin");
+    });
+  });
+  describe('User', () => {
+    describe('Get user', () => {});
+    describe('Edit user', () => {});
+  });
+  describe('Bookmark', () => {
+    describe('Get empty bookmarks', () => {});
+    describe('Create bookmark', () => {});
+    describe('Get bookmarks', () => {});
+    describe('Get bookmark by id', () => {});
+    describe('Update bookmark by id', () => {});
+    describe('Delete bookmark by id', () => {});
+  });
+
   it.todo('this should pass if all startup / teardown are working');
 });
